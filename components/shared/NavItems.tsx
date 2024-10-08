13c010825d3a55642400117e38af14561ec3c41d
Jace <contactjacenair@gmail.com>
2024-10-08 07:52:26 +0100

    dev from new pc

--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -1,7 +1,10 @@
 export default function NavItems() {
     return(
         <ul className="md:flex-between flex">
-
+            <li>one</li>
+            <li>two</li>
+            <li>three</li>
+            <li>four</li>
         </ul>
     );
 }
