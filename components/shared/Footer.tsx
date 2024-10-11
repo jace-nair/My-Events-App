64e4c81cd72418890809f2691484d7c1ca3b9d38
Jace <contactjacenair@gmail.com>
2024-10-11 05:24:56 +0100

    Add Footer details

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,5 +1,20 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Footer() {
     return(
-        <footer>Footer Section</footer>
+        <footer className="border-t">
+            <div className="wrapper flex flex-center flex-between flex-col g-4 p-5 text-center sm:flex-row">
+                <Link href="/">
+                    <Image 
+                        src="/assets/images/logo.svg"
+                        alt="logo"
+                        width={128}
+                        height={38}
+                    />
+                </Link>
+                <p>2024 My Events App. All Rights reserved.</p>
+            </div>
+        </footer>
     );
 }
